fee2ce62e62bee3f50a1b94185cfd408fab28c2e
Jemyishai <jesse.moskowitz@gmail.com>
2018-01-24 18:23:11 -0500

    rendered green cube against GL background

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as THREE from 'three';
+import ExpoTHREE from 'expo-three';
+import Expo from 'expo';
+console.disableYellowBox = true;
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Expo.GLView
+      ref={(ref)=> this._glView = ref}
+      style={{ flex:1 }}
+      onContextCreate={this._onGLContextCreate}
+      />
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+_onGLContextCreate = async (gl) => {
+  const arSession = await this._glView.startARSessionAsync();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+  const renderer = ExpoTHREE.createRenderer({ gl });
+  renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    gl.endFrameEXP()
+  }
+
+cube.rotation.x += 0.07;
+cube.rotation.y += 0.04;
+
+animate();
+  scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer)
+
+  }
+}
