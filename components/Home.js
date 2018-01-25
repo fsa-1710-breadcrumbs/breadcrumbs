b0f1f62625c44aaf846808097c4d1bf6054bc3a4
Shannen <shannen.ye@gmail.com>
2018-01-25 16:46:31 -0500

    created wireframed sphere

--- components/Home.js
+++ components/Home.js
@@ -31,6 +31,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
 import Expo from 'expo';
+import { SphereGeometry } from 'three';
 console.disableYellowBox = true;
 
 const styles = StyleSheet.create({
@@ -46,9 +47,11 @@ const styles = StyleSheet.create({
 export default class Home extends React.Component {
   render() {
     return (
+      //full screen view via expo
         <Expo.GLView
         ref={(ref)=> this._glView = ref}
         style={{ flex:1 }}
+        //onContextCreate takes a callback which recieves a gl object
         onContextCreate={this._onGLContextCreate}
         />
     );
@@ -56,28 +59,40 @@ export default class Home extends React.Component {
 
 
 _onGLContextCreate = async (gl) => {
+  //all scene stuff is via 3js
   const arSession = await this._glView.startARSessionAsync();
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+  const camera = ExpoTHREE.createARCamera(
+    arSession, gl.drawingBufferWidth , gl.drawingBufferHeight, 0.01, 1000);
   const renderer = ExpoTHREE.createRenderer({ gl });
   renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
+//next three lines are pure 3js
+// const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+// cube.position.z= -0.4;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+// wire framed sphere code
+const geometry = new THREE.SphereGeometry(0.15, 20, 20);
+const material = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+sphere.position.z = -1.2;
 
   const animate = () => {
     requestAnimationFrame(animate);
+    // cube.rotation.x += 0.07;
+    // cube.rotation.y += 0.04;
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
     renderer.render(scene, camera);
+    //same from 2js excpet expo requires the end of the frame explicitly
     gl.endFrameEXP()
   }
 
-cube.rotation.x += 0.07;
-cube.rotation.y += 0.04;
+
 
 animate();
   scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer)
