32aa68560e01dbe7ceb7ac0b1fa82869b059c4bd
Jemyishai <jesse.moskowitz@gmail.com>
2018-01-26 18:24:47 -0500

    mutiple coordinates

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,7 @@ import { StackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
 import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
-import Expo, {Location, Permissions, Platform} from 'expo';
+import Expo from 'expo';
 import { SphereGeometry } from 'three';
 console.disableYellowBox = true;
 
@@ -18,17 +18,6 @@ const styles = StyleSheet.create({
 });
 
 export default class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      location: null
-    };
-  }
-
-  componentWillMount() {
-    this._getGeoLocation();
-  }
-
   render() {
     return (
       //full screen view via expo
@@ -36,32 +25,19 @@ export default class Home extends React.Component {
         ref={(ref)=> this._glView = ref}
         style={{ flex:1 }}
         //onContextCreate takes a callback which recieves a gl object
-        onContextCreate={this._onGLContextCreate}
+        onContextCreate={ this._onGLContextCreate }
         />
     );
   }
 
-_getGeoLocation = async () => {
-  let { status } = await Permissions.askAsync(Permissions.LOCATION);
-  if (status === 'granted') {
-    let location = await Location.getCurrentPositionAsync( { enableHighAccuracy: true } );
-    this.setState( { location } );
-    console.log("this is my geo location: ", this.state.location)
-  }
-}
-
-
 _onGLContextCreate = async (gl) => {
   //all scene stuff is via 3js
-  // console.log("this is running inside onGLCONTEXTCREATE: ", gl)
   const arSession = await this._glView.startARSessionAsync();
   const scene = new THREE.Scene();
   const camera = ExpoTHREE.createARCamera(
     arSession, gl.drawingBufferWidth , gl.drawingBufferHeight, 0.01, 1000);
   const renderer = ExpoTHREE.createRenderer({ gl });
   renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-  // console.log("this is running: ", Expo.Location.getCurrentPositionAsync())
-  // console.log("this is running inside:")
 
 //next three lines are pure 3js
 // const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
@@ -73,10 +49,12 @@ _onGLContextCreate = async (gl) => {
 // wire framed sphere code
 const geometry = new THREE.SphereGeometry(0.15, 20, 20);
 const material = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
-const mesh = new Physijs.BoxMesh(geometry, material);
-scene.add(mesh);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+sphere.position.x = 0;
+sphere.position.y = -2;
+sphere.position.z = -2;
 
-sphere.position.z = 0;
 
   const animate = () => {
     requestAnimationFrame(animate);
