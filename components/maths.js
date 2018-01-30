7d7b6db26336d2f93ea3df47797d2ddcc97e41db
Jemyishai <jesse.moskowitz@gmail.com>
2018-01-29 22:32:39 -0500

    rollback to confirm geo coordinates are not properly being mapped

--- components/maths.js
+++ components/maths.js
@@ -51,3 +51,38 @@
 //  },
 //  "timestamp": 1517259129574.0671,
 //   }
+
+
+// // createCrumbs(la,lo){
+//   //  let la2 = 40.70499719400394;
+//    let la2 = 40.70489977297687;
+//   //  -74.00918462215232
+//   // let lo2 = -74.00868363151994;
+//   let lo2 = -74.00918462215232;
+// let geometry2 = new THREE.SphereGeometry(0.15, 20, 20);
+// let material2 = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
+// let sphere2 = new THREE.Mesh(geometry2, material2);
+// scene.add(sphere2);
+// maths(la2,lo2)
+// sphere2.position.x
+// console.log('sphere.position2.x', sphere2.position.x)
+// sphere2.position.y
+// console.log('sphere.position2.y', sphere2.position.y)
+// sphere2.position.z
+// console.log('sphere.position2.z', sphere2.position.z)
+// }
+
+
+// let la3 = 40.70496100586784;
+// let lo3 = -74.00935287303439;
+// let geometry3 = new THREE.SphereGeometry(0.15, 20, 20);
+// let material3 = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
+// let sphere3 = new THREE.Mesh(geometry3, material3);
+// scene.add(sphere3);
+// maths(la3,lo3)
+// sphere3.position.x
+// console.log('sphere.position3.x', sphere3.position.x)
+// sphere3.position.y
+// console.log('sphere.position3.y', sphere3.position.y)
+// sphere3.position.z
+// console.log('sphere.position3.z', sphere3.position.z)
