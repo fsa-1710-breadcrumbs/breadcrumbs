9ef50d50f1ccfb630774eefaac1326ac525815bd
Shannen <shannen.ye@gmail.com>
2018-01-30 19:08:22 -0500

    adjusted lat and long position

--- components/Home.js
+++ components/Home.js
@@ -18,39 +18,18 @@ const styles = StyleSheet.create({
 });
 
 export default class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      location: null
-        // location: {
-        //    "coords": {
-        //      "accuracy": 65,
-        //      "altitude": 8.645169258117676,
-        //      "altitudeAccuracy": 12.72334098815918,
-        //      "heading": -1,
-        //      "latitude": 40.70467336127081,
-        //      "longitude": -74.00862513042138,
-        //      "speed": -1,
-        //    },
-        //    "timestamp": 1516994126569.8108,
-        //  }
-    }
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+      // location: {
+      //   latitude: 40.70486772125275,
+      //   longitude: -74.00894022098937
+      // }
+    // }
+  // }
 
   componentWillMount() {
     this._getGeoLocation();
-    // this.state.location = {
-    //            "coords": {
-    //              "accuracy": 65,
-    //              "altitude": 8.645169258117676,
-    //              "altitudeAccuracy": 12.72334098815918,
-    //              "heading": -1,
-    //              "latitude": 40.70467336127081,
-    //              "longitude": -74.00862513042138,
-    //              "speed": -1,
-    //            },
-    //            "timestamp": 1516994126569.8108,
-    //          }
   }
 
   render() {
@@ -69,8 +48,12 @@ _getGeoLocation = async () => {
   let { status } = await Permissions.askAsync(Permissions.LOCATION);
   if (status === 'granted') {
     let location = await Location.getCurrentPositionAsync( { enableHighAccuracy: true } );
-    this.setState( { location } );
-    console.log("this is my geo location: ", this.state.location)
+    // Location.watchHeadingAsync(obj => console.log("this is obj: ", obj));
+
+    // Location.watchPositionAsync({enableHighAccuracy: true}, obj => console.log("this is postionAsync obj: ", obj));
+    // console.log("this is X: ", )
+    // this.setState( { location } );
+    // console.log("this is my geo location: ", this.state.location)
   }
 }
 
@@ -88,18 +71,58 @@ _onGLContextCreate = async (gl) => {
   const geometry = new THREE.SphereGeometry(0.15, 20, 20);
   const material = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
   const sphere = new THREE.Mesh(geometry, material);
+
+  // sphere.position.z = -1;
+  // sphere.position.x = -10;
+  let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+
+  const stateLat = location.coords.latitude;
+  const stateLon = location.coords.longitude;
+
+  const hallLat = 40.70480335751015;
+  const hallLon = -74.00931626986272;
+
+  const diffLat = stateLat - hallLat;
+  const diffLon = stateLon - hallLon;
+
+  const xCoord = diffLat * 110900;
+  // const zCoord = diffLon * 110900;
+  const zCoord = diffLon * 11090;
+
+
+  const position = new THREE.Vector3(xCoord, 0, zCoord);
+  let headings = await Location.getHeadingAsync();
+  let trueHeading = headings.trueHeading;
+  console.log('???????', position);
+  position.applyAxisAngle(new THREE.Vector3(0, 1, 0),(trueHeading * 0.01745329))
+  console.log("after ????????", position);
+  console.log("this is xz: ", xCoord, zCoord)
+
+  // sphere.translateX(xCoord);
+  // sphere.translateZ(-zCoord);
+  sphere.translateX(position.x);
+  sphere.translateZ(position.z);
+
+  // let headings = await Location.getHeadingAsync();
+  // let trueHeading = headings.trueHeading;
+  console.log("this is trueheading: ", trueHeading);
+  // sphere.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0),trueHeading * 0.01745329);
+
   scene.add(sphere);
 
-  const newSphereGeometry = new THREE.SphereGeometry(0.15, 20, 20);
-  const newSphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7fffd4, wireframe: true });
-  const newSphere = new THREE.Mesh(newSphereGeometry, newSphereMaterial);
-  scene.add(newSphere);
-  newSphere.position.z = -0.5;
+
+  // const newSphereGeometry = new THREE.SphereGeometry(0.15, 20, 20);
+  // const newSphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7fffd4, wireframe: true });
+  // const newSphere = new THREE.Mesh(newSphereGeometry, newSphereMaterial);
+  // scene.add(newSphere);
+  // newSphere.position.z = -0.5;
+
   // this.setTimeout(() => {
   //   newSphere.position.z--;
   // }, 3000);
 
-  sphere.position.z = 0;
+  // sphere.position.z = 0;
   // console.log("this is scene: ", scene)
 
   const animate = () => {
@@ -107,8 +130,8 @@ _onGLContextCreate = async (gl) => {
     sphere.rotation.x += 0.01;
     sphere.rotation.y += 0.01;
 
-    newSphere.rotation.x += 0.01
-    newSphere.rotation.y += 0.01
+    // newSphere.rotation.x += 0.01
+    // newSphere.rotation.y += 0.01
 
     renderer.render(scene, camera);
     gl.endFrameEXP()
