1b198ed3c1acb912ccd8257a9c1b212a914c2c99
Kevin Ho <ho.kevins@gmail.com>
2018-01-31 17:47:15 -0500

    changed IP.js to default localhost

--- IP.js
+++ IP.js
@@ -1,5 +1,5 @@
-// For Fullstack Academy
-const IP = 'http://172.16.21.157:1337';
+// For Default Localhost:
+const IP = 'http://127.0.0.1:1337';
 export default IP;
 
 // Click OPTION and click on the wireless icon ^ in the bar above to get your IP Address at home
