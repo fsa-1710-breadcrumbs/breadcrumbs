e7fc3cd87693f41bf5c0dcf0fdfd2fa753f334c0
Shannen <shannen.ye@gmail.com>
2018-02-01 12:55:25 -0500

    starting new trail

---
+++ Secrets.js
@@ -0,0 +1,4 @@
+process.env.GOOGLE_CLIENT_ID = '407740500546-3t9api3n3qv6tve7d2ujl5jvst1rsghp.apps.googleusercontent.com';
+process.env.GOOGLE_CLIENT_SECRET = 'e_w_13eoV_GsdGzuw5xo_Z7p';
+process.env.GOOGLE_CALLBACK = '/api/server/auth/google/';
+process.env.SESSION_SECRET = 'hanselandgretel';
