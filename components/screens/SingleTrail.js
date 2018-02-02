3f293ab8a3f32edec85e322c16e395296c9a7036
Jemyishai <jesse.moskowitz@gmail.com>
2018-02-02 11:02:59 -0500

    adjusted first and last coloring

--- components/screens/SingleTrail.js
+++ components/screens/SingleTrail.js
@@ -30,22 +30,21 @@ class SingleTrail extends React.Component {
     // trail array prop passed from Home component
     let trailToDisplay = this.props.navigation.state.params.breadcrumbs;
 
+    //first object rendered from trailToDisplay
+    let geometry = new THREE.SphereGeometry(0.2, 20, 20);
+    let material = new THREE.MeshBasicMaterial({ color: 0x2F5CFF, wireframe: true });
+    let sphere = new THREE.Mesh(geometry, material);
+    //xyz for first orb
+    sphere.position.x = trailToDisplay[0].x
+    sphere.position.y = trailToDisplay[0].y
+    sphere.position.z = trailToDisplay[0].z
+    scene.add(sphere)
+
     // creates a sphere for each object in trailToDisplay array except first && last
     let geometry2 = new THREE.SphereGeometry(0.1, 10, 10);
     let material2 = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
     //create xyz of each orb
-    for(let i = 1; i < trailToDisplay.length-1; i++){
-      if (i===0 || i === trailToDisplay.length-1){
-         //first object and last rendered from trailToDisplay
-        let geometry = new THREE.SphereGeometry(0.2, 20, 20);
-        let material = new THREE.MeshBasicMaterial({ color: 0x2F5CFF, wireframe: true });
-        let sphere = new THREE.Mesh(geometry, material);
-        //xyz for first orb
-        sphere.position.x = trailToDisplay[0].x
-        sphere.position.y = trailToDisplay[0].y
-        sphere.position.z = trailToDisplay[0].z
-        scene.add(sphere)
-      }
+    for(let i = 1; i <= trailToDisplay.length - 2; i++){
       let sphereMid = new THREE.Mesh(geometry2, material2);
       sphereMid.position.x = trailToDisplay[i].x;
       sphereMid.position.y = trailToDisplay[i].y;
@@ -53,6 +52,15 @@ class SingleTrail extends React.Component {
       scene.add(sphereMid);
       }
 
+    //last object rendered from trailToDisplay
+    let geometry3 = new THREE.SphereGeometry(0.2, 20, 20);
+    let material3 = new THREE.MeshBasicMaterial({ color: 0x2F5CFF, wireframe: true });
+    let sphere3 = new THREE.Mesh(geometry3, material3);
+    //xyz for last orb
+    sphere3.position.x = trailToDisplay[trailToDisplay.length - 1].x
+    sphere3.position.y = trailToDisplay[trailToDisplay.length - 1].y
+    sphere3.position.z = trailToDisplay[trailToDisplay.length - 1].z
+    scene.add(sphere3)
 
     animate();
     scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer)
