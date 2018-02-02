3fd0ed56528dfb7d0bd115f785c5437e3b759fe7
Vanessa Jimenez <vjimenez127@gmail.com>
2018-02-02 15:53:56 -0500

    Adding in a origin and destination

--- IP.js
+++ IP.js
@@ -1,11 +1,7 @@
 // FOR YOUR HOME IP, ADD INTO THIS FILE AND COMMENT OUT AND PUSH ONTO YOUR BRANCH SO THAT WE HAVE A COLLECTION OF IP ADDRESSES TO COMMENT AND USE:
 
 // For Fullstack Academy
-<<<<<<< HEAD
 const IP = 'http://172.16.22.137:1337';
-=======
-const IP = 'http://172.16.22.228:1337';
->>>>>>> db89b4ca8968f98321c2ac9daf74ae8d26fc503c
 
 // For Default Localhost:
 // const IP = 'http://127.0.0.1:1337';
