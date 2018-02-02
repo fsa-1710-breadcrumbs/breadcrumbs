62fa285123f22e726fd4aba45c7aa417651c0d8c
Shannen <shannen.ye@gmail.com>
2018-02-02 12:15:29 -0500

    added rotation to sphere

--- components/screens/StartTrail.js
+++ components/screens/StartTrail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
-import Expo, {Location, Permissions, Platform} from 'expo';
+import Expo from 'expo';
 import { SphereGeometry } from 'three';
-import { addTrail, create } from '../redux/trails';
+import { addTrail } from '../redux/trails';
 console.disableYellowBox = true;
 
 const styles = StyleSheet.create({
@@ -25,77 +25,31 @@ class Create extends React.Component {
     this.state = {
       breadcrumbs: [],
       stopInterval: null,
-      origin: '',
-      destination: '',
+      origin: null,
+      destination: null,
       userId: null
     }
-    this.x = [];
+    this.relativeLocation = [];
     this._onGLContextCreate = this._onGLContextCreate.bind(this);
   }
 
-  // componentWillMount() {
-  //   this._getGeoLocation();
-  // }
-  componentDidMount(){
-    console.log('this mounted on the dom');
-    //logic for running interval.
-    // this.setState({
-    //   stopInterval:
-    // })
-  }
-
-
   componentWillUnmount(){
     clearInterval(this.state.stopInterval);
-    // console.log("this is breadcrumbs before state: ", this.state.breadcrumbs);
-    // console.log("this is breadcrumbs before x thing: ", this.x);
-
-    // this.setState({
-    //   breadcrumbs: this.x,
-    //   origin: 'ORIGIN DEFAULT',
-    //   destination: 'DESTINATION DEFAULT',
-    //   userId: 5
-    // });
-
-    // console.log("this is breadcrumbs after state: ", this.state.breadcrumbs);
-    // console.log("this is breadcrumbs after x thing: ", this.x);
-
     this.props.createTrail({
-      breadcrumbs: this.x,
-      origin: 'DEFAULT',
-      destination: 'DEFAULT',
+      breadcrumbs: this.relativeLocation,
       userId: this.props.currentUser.id
     });
   }
 
-
-
   render() {
-    console.log('we should only see this once');
-    // console.log('this is the changing state: ', this.state.relativeLocation);
     return (
-      //full screen view via expo
       <Expo.GLView
       ref={(ref)=> this._glView = ref}
       style={{ flex:1 }}
-      //onContextCreate takes a callback which recieves a gl object
       onContextCreate={this._onGLContextCreate}
       />
     );
   }
-
-// _getGeoLocation = async () => {
-  // let { status } = await Permissions.askAsync(Permissions.LOCATION);
-  // if (status === 'granted') {
-  //   let location = await Location.getCurrentPositionAsync( { enableHighAccuracy: true } );
-    // Location.watchHeadingAsync(obj => console.log("this is obj: ", obj));
-
-    // Location.watchPositionAsync({enableHighAccuracy: true}, obj => console.log("this is postionAsync obj: ", obj));
-    // console.log("this is X: ", )
-    // this.setState( { location } );
-    // console.log("this is my geo location: ", this.state.location)
-//   }
-// }
 
 _onGLContextCreate = async (gl) => {
   const arSession = await this._glView.startARSessionAsync();
@@ -105,34 +59,37 @@ _onGLContextCreate = async (gl) => {
   const renderer = ExpoTHREE.createRenderer({ gl });
   renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-  const geometry = new THREE.SphereGeometry(0.15, 20, 20);
-  const material = new THREE.MeshBasicMaterial({ color: 0xee82ee, wireframe: true });
+  const geometry = new THREE.SphereGeometry(0.1, 30, 30);
+  const material = new THREE.MeshBasicMaterial({ color: 0x2F5CFF, wireframe: true });
   const sphere = new THREE.Mesh(geometry, material);
   sphere.position.z = -1;
   scene.add(sphere);
 
   const start = setInterval(() => {
-    let vectorPosition = camera.getWorldPosition()
+    let vectorPosition = camera.getWorldPosition();
     let newState = {
       x: vectorPosition.x,
       y: vectorPosition.y,
       z: vectorPosition.z,
     };
-   this.x.push(newState)
+   this.relativeLocation.push(newState)
   }, 2000);
 
   const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    gl.endFrameEXP()
+    gl.endFrameEXP();
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
   }
   animate();
 
   this.setState({
     stopInterval: start
-  })
+  });
 
-  scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer)
+  scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
   }
 }
 
