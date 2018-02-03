f5cbd634004c12b0a1631ef8175beddb0b67befd
Vanessa Jimenez <vjimenez127@gmail.com>
2018-02-03 13:38:28 -0500

    ip

--- IP.js
+++ IP.js
@@ -1,11 +1,8 @@
 // FOR YOUR HOME IP, ADD INTO THIS FILE AND COMMENT OUT AND PUSH ONTO YOUR BRANCH SO THAT WE HAVE A COLLECTION OF IP ADDRESSES TO COMMENT AND USE:
 
 // For Fullstack Academy
-<<<<<<< HEAD
 const IP = 'http://172.16.22.137:1337';
-=======
-const IP = 'http://172.16.21.157:1337';
->>>>>>> 69adf86d5fb84601336dd5e46b2314825c49c602
+
 
 // For Default Localhost:
 // const IP = 'http://127.0.0.1:1337';
