c1ca92c484b264fbbad9e860af6fbde5af614322
Jemyishai <jesse.moskowitz@gmail.com>
2018-02-06 10:52:35 -0500

    jesseLocal IP added

--- IP.js
+++ IP.js
@@ -1,6 +1,7 @@
 // FOR YOUR HOME IP, ADD INTO THIS FILE AND COMMENT OUT AND PUSH ONTO YOUR BRANCH SO THAT WE HAVE A COLLECTION OF IP ADDRESSES TO COMMENT AND USE:
 
-const IP = 'https://breadcrumbs.herokuapp.com';
+// const IP = 'https://breadcrumbs.herokuapp.com';
+const IP = 'http://172.16.22.221:1337';
 export default IP;
 
 // Click OPTION and click on the wireless icon ^ in the bar above to get your IP Address at home
