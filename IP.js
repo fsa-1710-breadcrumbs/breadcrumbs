ca0065bd5e88b0a2849dba85121b88635d0f1828
Kevin Ho <ho.kevins@gmail.com>
2018-02-07 13:45:22 -0500

    changed readme

--- IP.js
+++ IP.js
@@ -3,4 +3,4 @@ const IP = 'https://breadcrumbs.herokuapp.com';
 export default IP;
 
 // Click OPTION and click on the wireless icon ^ in the bar above to get your IP Address at home
-// -- Make sure to keep the PORT 1337 to use i.e. 'http://000.000.000.000:1337'
+// -- Make sure to keep the PORT 1337 to use i.e. 'http://000.000.000.000:1337' and replace on Line 1 above.
